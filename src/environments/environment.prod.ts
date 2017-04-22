bcbd23a0bff0529f0b7eae60e8a55b28a8a51eb5
Alexander Marchenko <maappdev@gmail.com>
2017-04-22 12:00:00 +0300

    Fix env values

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,9 @@
 export const environment = {
-  production: true
+  production: true,
+  envName: 'prod',
+  appName: 'QAuction Platform',
+  cognito: {
+    poolId: 'eu-west-1_R1ZMBZJeB',
+    appClientId: '1qrjc2hbf9nmfbj4abc1iph75n'
+  }
 };
